3b651dc5efe56152b209fe33c36e818ef653f36b
Sam Ryan <samjosephryan@gmail.com>
2016-08-20 15:51:26 -0400

    Made some more changes to the questions you can ask

--- generator.py
+++ generator.py
@@ -6,19 +6,20 @@
 
 def bot(sentence) :
     
-    #unwanted chars are removed
-    unwatedChars = "!@#$?,." #if users  
-    for char in unwatedChars: #uses for to loop through input
-        sentence = sentence.replace(char,"") #gets rid of unwanted characters
-   
-    #create lists of words that might be asked
-    welcomeDict = ['hello', 'hi', 'welcome', 'hey', 'whats up' ] #creating a welcome dictionary
-    healthDict = ['how', 'hows', 'are', 'you', 'hey', 'whats', 'up' ] #creating a asking how you are doing dictionary
-    payoutDict = ['payout', 'payment', 'pay', 'money', 'my', ] # dictionary for payment
+    unwantedChars = "!@#$?,." #unwanted chars are removed
 
-    #split up the sentence into an array
+    for char in unwantedChars: # uses a for loop to go through the input
+        sentence = sentence.replace(char,"") # gets rid of unwanted characters
+
+    #split up the sentence into an array and make all characters lower case
+    sentence = sentence.lower()
     sentence = sentence.split()
-
+ 
+    #create lists of words that might be asked
+    welcomeDict = ['hello', 'hi', 'welcome', 'hey', 'whats up' ] # creating a welcome dictionary
+    healthDict = ['how', 'hows', 'are', 'you', 'hey', 'whats', 'up' ] # creating a asking how you are doing dictionary
+    payoutDict = ['payout', 'payment', 'pay', 'money', 'my', ] # dictionary for payment
+   
     #checks for any common values
     welcomeResult = len(set(welcomeDict).intersection(sentence))
     healthResult = len(set(healthDict).intersection(sentence))
@@ -31,11 +32,13 @@ def bot(sentence) :
     if healthResult > 1 :
 	 botResponse.append('I am well, thanks! ')
     if payoutResult > 1 :
-	 botResponse.append('We make the payout on the 1st ')
+	 botResponse.append('We make the payout on the 1st. ')
+
+    botResponse.append('If you still have any questions send us an email. ')
     
     finalBotResponse = ''.join(botResponse) # joins the array into a string
     
     return finalBotResponse # final response from bot
 
-print bot('How are you? When is my payout')
+print bot('How are you. I need my payout')
 
